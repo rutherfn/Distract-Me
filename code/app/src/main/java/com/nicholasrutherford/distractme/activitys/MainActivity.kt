4b3287d8d6175eafc3cce6f36f10074fe46e7295
Nicholas Rutherford <nicholasrutherford@Nicholass-Air.attlocal.net>
2020-03-21 12:14:38 -0500

    added splash activity, still need to add layout and functionality to splash screen

--- code/app/src/main/java/com/nicholasrutherford/distractme/activitys/MainActivity.kt
+++ code/app/src/main/java/com/nicholasrutherford/distractme/activitys/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.nicholasrutherford.distractme
+package com.nicholasrutherford.distractme.activitys
 
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -6,8 +6,7 @@ import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import com.nicholasrutherford.distractme.R
 import com.nicholasrutherford.distractme.ui.main.SectionsPagerAdapter
 
 class MainActivity : AppCompatActivity() {
---
+++ code/app/src/main/java/com/nicholasrutherford/distractme/activitys/SplashActivity.kt
@@ -0,0 +1,6 @@
+package com.nicholasrutherford.distractme.activitys
+
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+}
