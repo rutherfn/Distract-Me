7f16bd9473761e935c5856eb7f3c1d989ff6ce54
Nicholas Rutherford <nicholasrutherford@Nicholass-Air.attlocal.net>
2020-03-21 16:06:36 -0500

    added base func for view pager, and test fragment is created that has correct layout needed. Also added a nested view pager

--- code/app/src/main/java/com/nicholasrutherford/distractme/adapters/ViewPagerAdapter.kt
+++ code/app/src/main/java/com/nicholasrutherford/distractme/adapters/ViewPagerAdapter.kt
@@ -1,16 +1,23 @@
 package com.nicholasrutherford.distractme.adapters
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import androidx.fragment.app.FragmentStatePagerAdapter
+import com.nicholasrutherford.distractme.R
 import com.nicholasrutherford.distractme.fragments.Home
 import java.util.HashMap
 
-class ViewPager (private val mFragmentManager: FragmentManager) : FragmentPagerAdapter(mFragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+class ViewPagerAdapter (private val mFragmentManager: FragmentManager, private val mContext: Context) : FragmentPagerAdapter(mFragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
 
-    private val mFragmentTags: MutableMap<Int, String?> = TODO()
+    private val mFragmentTags: MutableMap<Int, String?>
+    private val TAB_TITLES = arrayOf(
+        R.string.tab_text_1,
+        R.string.tab_text_2
+    )
 
     override fun getItem(position: Int): Fragment {
         return when (position) {
@@ -21,6 +28,10 @@ class ViewPager (private val mFragmentManager: FragmentManager) : FragmentPagerA
 
     override fun getCount(): Int {
         return 1
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return mContext.resources.getString(TAB_TITLES[position])
     }
 
     private fun getHomeInstance() = Home()
--- code/app/src/main/java/com/nicholasrutherford/distractme/fragments/Home.kt
+++ code/app/src/main/java/com/nicholasrutherford/distractme/fragments/Home.kt
@@ -4,17 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.nicholasrutherford.distractme.R
 import com.nicholasrutherford.distractme.adapters.Recyclers.Article
-import kotlinx.android.synthetic.main.fragment_home.*
 
 class Home : Fragment() {
-
     private var mView: View? = null
-    private var articleAdapter: Article? = null
+    private var rvHomes: RecyclerView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,6 +20,7 @@ class Home : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         mView = inflater.inflate(R.layout.fragment_home, container, false)
+        rvHomes = mView!!.findViewById(R.id.rvHome)
         main()
         return mView
     }
@@ -31,8 +30,17 @@ class Home : Fragment() {
     }
 
     private fun setUpArticleAdapter() {
-        rvHome.itemAnimator = null
-        rvHome.layoutManager = LinearLayoutManager(context)
-        rvHome.adapter = articleAdapter
+        val articleAdapter = Article(context!!)
+        rvHomes!!.itemAnimator = null
+        rvHomes!!.layoutManager = LinearLayoutManager(context!!)
+        rvHomes!!.adapter = articleAdapter
+    }
+
+    fun newInstance(title: String): Fragment {
+        val fragment = Home()
+        val args = Bundle()
+        args.putString("title", title)
+        fragment.arguments = args
+        return fragment
     }
 }
