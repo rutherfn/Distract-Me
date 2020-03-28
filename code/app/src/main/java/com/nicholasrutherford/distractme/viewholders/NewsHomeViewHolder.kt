545ad463b85c6df7a3450f2195c7f110f4cd4913
Nicholas Rutherford <nicholasrutherford@Nicholass-Air.attlocal.net>
2020-03-28 00:25:24 -0500

    set data from network call to recycler view holder

--- code/app/src/main/java/com/nicholasrutherford/distractme/viewholders/NewsHomeViewHolder.kt
+++ code/app/src/main/java/com/nicholasrutherford/distractme/viewholders/NewsHomeViewHolder.kt
@@ -2,13 +2,31 @@ package com.nicholasrutherford.distractme.viewholders
 
 import android.content.Context
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.nicholasrutherford.distractme.R
 import com.nicholasrutherford.distractme.data.NewsResponse
+import com.nicholasrutherford.distractme.helpers.Typeface
+import com.squareup.picasso.Picasso
+import de.hdodenhof.circleimageview.CircleImageView
 
 class NewsHomeViewHolder(itemView: View, private val mContext: Context) : RecyclerView.ViewHolder(itemView) {
 
+    private val typeface = Typeface()
+    private var ivArticle: CircleImageView = itemView.findViewById(R.id.ivArticle)
+    private var tvArticleTitle: TextView = itemView.findViewById(R.id.tvArticleTitle)
+    private var tvArticleDesc: TextView = itemView.findViewById(R.id.tvArticleDesc)
+
     fun main(newsResponse: NewsResponse, pos: Int) {
-        // func goes here
+        setTypeFace()
+        Picasso.get().load(newsResponse.articles[pos].urlToImage).into(ivArticle)
+        tvArticleTitle.text = newsResponse.articles[pos].title
+        tvArticleDesc.text = newsResponse.articles[pos].description
+    }
+
+    private fun setTypeFace() {
+        typeface.setTypefaceForHeaderBold(tvArticleTitle,mContext)
+        typeface.setTypefaceForBodyRegular(tvArticleDesc,mContext)
     }
 
 }
