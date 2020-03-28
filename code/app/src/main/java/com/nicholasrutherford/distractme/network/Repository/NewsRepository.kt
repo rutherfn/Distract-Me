3ebbd53d947e0dc8fd072c7af19dcc4ec3def5e9
Nicholas Rutherford <nicholasrutherford@Nicholass-Air.attlocal.net>
2020-03-28 02:26:56 -0500

    added suspend method func, to use with live data. Super cool stuff

--- code/app/src/main/java/com/nicholasrutherford/distractme/network/Repository/NewsRepository.kt
+++ code/app/src/main/java/com/nicholasrutherford/distractme/network/Repository/NewsRepository.kt
@@ -1,10 +1,9 @@
 package com.nicholasrutherford.distractme.network.Repository
 
-import androidx.lifecycle.LiveData
 import com.nicholasrutherford.distractme.data.NewsResponse
 
 interface NewsRepository {
 
-    fun getNewsTopHeadlines(country: String, apiKey: String): LiveData<NewsResponse>
+    suspend fun getNewsTopHeadlines(country: String, apiKey: String): NewsResponse
 
 }
--- code/app/src/main/java/com/nicholasrutherford/distractme/network/RepositoryImp/NewsRepositoryImp.kt
+++ code/app/src/main/java/com/nicholasrutherford/distractme/network/RepositoryImp/NewsRepositoryImp.kt
@@ -1,36 +1,14 @@
 package com.nicholasrutherford.distractme.network.RepositoryImp
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.nicholasrutherford.distractme.data.NewsResponse
 import com.nicholasrutherford.distractme.network.NewsApiService
 import com.nicholasrutherford.distractme.network.Repository.NewsRepository
 import com.nicholasrutherford.distractme.network.RetrofitClient
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class NewsRepositoryImp: NewsRepository  {
     private var client: NewsApiService = RetrofitClient.RetroFitClient.retrofitNewsService
 
-    override fun getNewsTopHeadlines(country: String, apiKey: String): LiveData<NewsResponse> {
-        val liveData = MutableLiveData<NewsResponse>()
-
-        client.getTopHeadlines(country, apiKey).enqueue(object:
-            Callback<NewsResponse> {
-            override fun onResponse(call: Call<NewsResponse>, response: Response<NewsResponse>) {
-                if (response.isSuccessful) {
-
-                    // When data is available, populate LiveData
-                    liveData.value = response.body()
-                }
-            }
-
-            override fun onFailure(call: Call<NewsResponse>, t: Throwable) {
-                t.printStackTrace()
-            }
-        })
-
-        return liveData
+    override suspend fun getNewsTopHeadlines(country: String, apiKey: String): NewsResponse {
+        return client.getTopHeadlines (country, apiKey)
     }
 }
